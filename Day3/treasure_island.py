850ee358874d5e09245c11c1cb8889ea78d33273
LySerigne <46936745+LySerigne@users.noreply.github.com>
2020-11-30 13:13:19 +0100

    Create treasure_island.py
    A treasure island game

---
+++ Day3/treasure_island.py
@@ -0,0 +1,49 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+# You can find the flowchart in the following link
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+cross_road = input("You are at a cross road. Where do you want to go? type 'left' or 'right' ").lower()
+
+if cross_road == 'left':
+  swim_wait = input("swim or wait? ").lower()
+  if swim_wait == 'swim':
+    print("Attacked by trout. \nGame over:)")
+  elif swim_wait == 'wait':
+    door = input("Which door? type 'blue', 'yellow' or 'red' ").lower()
+    
+    if door == 'blue':
+     print("Eating by beasts.\nGame over :)")
+    elif door == 'red':
+     print("Burned by fire.\n Game over :)")
+    elif door == 'yellow':
+     print("Congratulation. You win!!")
+  else:
+     print("Game over :)")
+else:
+  print("Fall into a hole. Game Over :)")
+
+
