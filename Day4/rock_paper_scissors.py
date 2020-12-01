e535614a22ee7349ed576b69b2f6dc8a2d271bc5
LySerigne <46936745+LySerigne@users.noreply.github.com>
2020-12-01 18:59:27 +0100

    Update rock_paper_scissors.py

--- Day4/rock_paper_scissors.py
+++ Day4/rock_paper_scissors.py
@@ -45,29 +45,20 @@ else:
 
 
   if user_choice == 0 and computer_choice == 2:
-  #print(f"You \n{rock}")
-  #print(f"Computer \n{scissors}")
     print("Congratulation. You Rock!")
-
   elif user_choice == 1 and computer_choice == 0:
-  #print(f"You \n{paper}")
-  #print(f"Computer \n{rock}")
     print("Congratulation. You win!")
-
   elif user_choice == 2 and computer_choice == 1:
- #print(f"You \n{scissors}")
-  #print(f"Computer \n{paper}")
     print("You win!")
-
   elif user_choice == 2 and computer_choice == 0:
-  #print(f"You \n{scissors}")
-  #print(f"Computer \n{rock}")
-    print("Congratulation. You Rock!")
-
+    print("You lose!")
   elif computer_choice == user_choice:
     print("It's a drawn. Try again")
 
   elif computer_choice > user_choice:
-  #print(f"You \n{paper}")
-  #print(f"Computer \n{scissors}")
     print("Computer wins")
+
+
+
+
+
