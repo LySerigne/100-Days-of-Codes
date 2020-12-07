32cd6d62876695a363c65ac65a4bc4de206a4647
LySerigne <46936745+LySerigne@users.noreply.github.com>
2020-12-07 10:35:15 +0100

    Create average_heght.py

---
+++ Day5/average_heght.py
@@ -0,0 +1,19 @@
+# Implementing the average function
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+
+total_height = 0
+for height in student_heights:
+  total_height += height
+  
+students = 0
+for student in student_heights:
+  students += 1
+
+average = total_height / students
+print(students)
+print(f"The average height is {round(average)}")
+
+
+
